ec4738d36e05e4ce72be0f49f7ec87db4ace5171
max  muller <maxsovc3@gmail.com>
2022-04-25 21:47:05 -0300

    Utilizando Amazon como repositório e alterando arquivos de media e estáticos.

--- projeto_final/settings.py
+++ projeto_final/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
@@ -141,3 +140,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     'statics',
 ]
+
+AWS_ACCESS_KEY_ID = config('AKIAW5D4KBQNF66S7C6Z')
+AWS_SECRET_ACCESS_KEY = config ('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')
+AWS_STORAGE_BUCKET_NAME = 'mybucket-django'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
